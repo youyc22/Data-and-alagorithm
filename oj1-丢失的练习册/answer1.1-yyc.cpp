bd2541e34c7bf018440eab79b375aa8b0278580d
Yichen You <125688164+youyc22@users.noreply.github.com>
2023-10-24 00:01:06 +0800

    Create answer1.1-yyc.cpp

---
+++ oj1-丢失的练习册/answer1.1-yyc.cpp
@@ -0,0 +1,38 @@
+//二进制按位相加后模3
+
+#include <stdio.h>
+#pragma warning (disable:4996)
+
+int pow(int a, int n)
+{
+	int r = 1;
+	for (int i = 0; i < n; i++)
+	{
+		r *= a;
+	}
+	return r;
+}
+int main()
+{
+	int N = 0;
+	int number = 0;
+	int result=0;
+	scanf("%d", &N);
+	int a[32] = { 0 };
+	for (int j = 0; j < 3 * N - 1; j++)
+	{
+		scanf("%d", &number);
+		for (int i = 0; i < 32; i++)
+		{
+			a[i] += (number >> i) & 1;
+		}
+	}
+	for (int i = 0; i < 32; i++)
+	{
+		if (a[i] % 3 != 0)
+		{
+			result += pow(2, i);
+		}
+	}
+	printf("%d", result);
+}
